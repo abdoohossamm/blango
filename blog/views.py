7c38518d57610d70f8482ffb2840dc886e104d74
Abdalrahman Mohamed <abdalrahmanmohamed@my.uopeople.edu>
2022-05-10 23:45:01 +0000

    Finish database optimization

--- blog/views.py
+++ blog/views.py
@@ -9,10 +9,16 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
+def get_ip(request):
+  from django.http import HttpResponse
+  return HttpResponse(request.META['REMOTE_ADDR'])
+
+
 # @cache_page(300)
 # @vary_on_cookie
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
