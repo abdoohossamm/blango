cae111c16a89be082e7a8ad15fc7224bd9406f5f
Abdalrahman Mohamed <abdalrahmanmohamed@my.uopeople.edu>
2022-06-15 22:50:34 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -58,7 +58,9 @@ class Dev(Configuration):
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
         "rest_framework",
+        'rest_framework.authtoken',
     ]
+    
     INTERNAL_IPS = ["192.168.10.93"]
     MIDDLEWARE = [
         'debug_toolbar.middleware.DebugToolbarMiddleware',
@@ -188,6 +190,13 @@ class Dev(Configuration):
     ACCOUNT_EMAIL_REQUIRED = True
     ACCOUNT_USERNAME_REQUIRED = False
     ACCOUNT_AUTHENTICATION_METHOD = "email"
+    REST_FRAMEWORK = {
+      "DEFAULT_AUTHENTICATION_CLASSES": [
+          "rest_framework.authentication.BasicAuthentication",
+          "rest_framework.authentication.SessionAuthentication",
+          "rest_framework.authentication.TokenAuthentication",
+      ]
+    }
 class Prod(Dev):
   DEBUG = False
   SECRET_KEY = values.SecretValue()
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.authentication import SessionAuthentication
 
 from blog.api.serializers import PostSerializer
 from blog.models import Post
@@ -6,6 +7,7 @@ from blog.models import Post
 # API views with generics views and mixins
 
 class PostList(generics.ListCreateAPIView):
+    # authentication_classes = [SessionAuthentication]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
